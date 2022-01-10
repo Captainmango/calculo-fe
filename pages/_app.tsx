1b5383faf6091f9180d05a0f13ad8032e411d473
Edward Heaver <edward.heaver01@gmail.com>
2022-01-10 20:16:30 +0000

    Moved to material UI and tested. Bit slower, but seems OK

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import type { AppProps } from 'next/app'
-import 'materialize-css/dist/css/materialize.min.css'
 import Head from 'next/head'
+import React from 'react';
+import { CssBaseline } from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      //@ts-ignore
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (<>
+  
     <Head>
       <title>Calculo</title>            
     </Head>
+    <CssBaseline />
     <Component {...pageProps} />
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,35 @@
+import { Container, TextField } from '@mui/material';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { UserState, UserStore } from '../store/UserAuthStore'
+import DateAdapter from '@mui/lab/AdapterDayjs';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { TimePicker } from '@mui/lab';
+import { useState } from 'react';
 
-
-const Home: NextPage = () => {
+const Index: NextPage = () => {
   //@ts-ignore
   const [useStore, api] = UserStore
+  const [time, setTime] = useState(null)
 
   function testfunc() {
     api.dispatch({ type: "LOG_IN", body: { user: null, loading: true } })
   }
-  return (<>
-    <h1 className='card-panel teal lighten-2 center-align'>Test entry</h1>
-    <button onClick={() => testfunc()}>Click me</button>
-    <p>{useStore((state: UserState)=>state.loading)}</p>
-  </>
-  )
-}
+  return (
 
-export default Home
+    <Container>
+      <LocalizationProvider dateAdapter={DateAdapter}>    
+      <TimePicker
+        label="Basic example"
+        value={time}
+        onChange={(newValue) => {
+          setTime(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  </Container>
+  )
+};
+
+export default Index
