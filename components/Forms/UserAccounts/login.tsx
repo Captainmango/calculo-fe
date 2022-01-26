31addacf229f2d3bddd9ba3cd143aa33441fe951
Edward Heaver <edward.heaver01@gmail.com>
2022-01-26 17:41:21 +0000

    Tidy up

--- components/Forms/UserAccounts/login.tsx
+++ components/Forms/UserAccounts/login.tsx
@@ -2,32 +2,32 @@ import { FormControl, FormHelperText, FormLabel, Input, Box, Button, forwardRef,
 import Link from 'next/link'
 import React from 'react'
 
-const LoginForm =forwardRef<BoxProps, 'div'> ((props, ref) => (
-        <Box w="80%">
-            <form>
-                <FormControl py="8">
-                    <FormLabel htmlFor='email' color="white">Email address</FormLabel>
-                    <Input variant="flushed" id='email' type='email' color="white" />
-                    <FormHelperText color="white">Your email will never be shared</FormHelperText>
-                </FormControl>
-                <FormControl>
-                    <FormLabel htmlFor='password' color="white">Password</FormLabel>
-                    <Input variant="flushed" id='password' type='password' color="white" />
-                </FormControl>
-                <Box w="100%" pt="8" display="flex" justifyContent="space-evenly">
-                    <Link href="/sign-up" passHref>
-                        <Button colorScheme="whiteAlpha" variant="outline">
-                            Sign up
-                        </Button>
-                    </Link>
-
-                    <Button colorScheme="whatsapp">
-                        Log in
+const LoginForm = () => (
+    <Box w="80%">
+        <form>
+            <FormControl py="8">
+                <FormLabel htmlFor='email' color="white">Email address</FormLabel>
+                <Input variant="flushed" id='email' type='email' color="white" />
+                <FormHelperText color="white">Your email will never be shared</FormHelperText>
+            </FormControl>
+            <FormControl>
+                <FormLabel htmlFor='password' color="white">Password</FormLabel>
+                <Input variant="flushed" id='password' type='password' color="white" />
+            </FormControl>
+            <Box w="100%" pt="8" display="flex" justifyContent="space-evenly">
+                <Link href="/sign-up" passHref>
+                    <Button colorScheme="whiteAlpha" variant="outline">
+                        Sign up
                     </Button>
+                </Link>
 
-                </Box>
-            </form>
-        </Box>
-    ))
+                <Button colorScheme="whatsapp">
+                    Log in
+                </Button>
+
+            </Box>
+        </form>
+    </Box>
+)
 
 export default LoginForm
