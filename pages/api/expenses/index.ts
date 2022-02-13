051d06a2bb08202f4240ea1097fbc72f4ba39a93
Edward Heaver <edward.heaver01@gmail.com>
2022-02-13 21:10:31 +0000

    removed dead import

--- pages/api/expenses/index.ts
+++ pages/api/expenses/index.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { CategoryEnum } from "../../../models/CategoryEnum"
 import { Expense } from "../../../models/Expense"
 import expensesArray from '../../../resources/Calculo_expenses.json'
-import { ActionTypes, useAppStore } from "../../../store"
 
 export default async function handler(
     req: NextApiRequest,
